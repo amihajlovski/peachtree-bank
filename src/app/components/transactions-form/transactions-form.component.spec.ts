f3bf9adbb5e951f31dc7c174f10111e3abb0e8ea
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2020-09-23 16:58:21 +0200

    check in test if button exists

--- src/app/components/transactions-form/transactions-form.component.spec.ts
+++ src/app/components/transactions-form/transactions-form.component.spec.ts
@@ -3,6 +3,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { TransactionsFormComponent } from './transactions-form.component';
 
@@ -31,5 +32,11 @@ describe('TransactionsFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have submit button', () => {
+    const button = fixture.debugElement.query(By.css('button'));
+    const buttonEl = button.nativeElement;
+    expect(buttonEl).toBeDefined();
   });
 });
